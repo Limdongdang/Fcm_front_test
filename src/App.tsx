17fa1ce1db7ebf473a925987db2697a7c8850ba0
LimDong <50188317+Limdongdang@users.noreply.github.com>
2024-04-11 05:24:01 +0900

    fcm 1차 메시지 테스트 완료

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import '../firebase/firebase_initialize_app.js';
+// import '../firebase/messaging_get_token.js';
+function App() {
+    const [count, setCount] = useState(0);
+    requestPermission();
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        </>
+    );
+}
+function requestPermission() {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+        if (permission === 'granted') {
+            console.log('Notification permission granted.');
+        }
+    });
+}
+export default App;
